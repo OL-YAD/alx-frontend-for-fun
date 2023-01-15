b4783fb4694247e793b83db73bcf52d8a001c0ae
OL-YAD <olyadtemesgen@gmail.com>
2023-01-15 17:40:36 +0300

    markdown

--- markdown2html.py
+++ markdown2html.py
@@ -23,16 +23,29 @@ if __name__ == "__main__":
     with mdfile as md:
         with htmlfile as html:
             ulExists = False
+            olExists = False
+            parExists = False
             for line in mdfile:
+                line = line.replace("__", "<em>", 1)
+                line = line.replace("__", "</em>", 1)
+                line = line.replace("**", "<b>", 1)
+                line = line.replace("**", "</b>", 1)
+
                 lenOfLine = len(line)
                 heading = line.lstrip("#")
                 numOfHeading = lenOfLine - len(heading)
                 ulListItem = line.lstrip("-")
                 ulItem = lenOfLine - len(ulListItem)
+                olListItem = line.lstrip("*")
+                olItem = lenOfLine - len(olListItem)
+                bold = line.lstrip("**")
+                boldItem = lenOfLine - len(bold)
+                emph = line.lstrip("__")
+                emphItem = lenOfLine - len(emph)
 
                 if 1 <= numOfHeading <= 6:
                     line = "<h{}>".format(numOfHeading) + heading.strip() + "</h{}>\n".format(numOfHeading)
-              
+
                 if ulItem:
                     if not ulExists:
                         html.write("<ul>\n")
@@ -42,8 +55,31 @@ if __name__ == "__main__":
                     html.write("</ul>\n")
                     ulExists = False
 
+                if olItem:
+                    if not olExists:
+                        html.write("<ol>\n")
+                        olExists = True
+                    line = "<li>" + olListItem.strip() + "</li>\n"
+                if olExists and not olItem:
+                    html.write("</ol>\n")
+                    olExists = False
+
+                if not numOfHeading and not ulItem and not olItem:
+                    if not parExists and lenOfLine > 1:
+                        html.write("<p>\n")
+                        parExists = True
+                    elif lenOfLine > 1:
+                        html.write("<br/>\n")
+                    elif parExists:
+                        html.write("</p>\n")
+                        parExists = False
+
                 if lenOfLine > 1:
                     html.write(line)
 
             if ulExists:
                 html.write("</ul>\n")
+            if olExists:
+                html.write("</ol>\n")
+            if parExists:
+                html.write("</p>\n")
