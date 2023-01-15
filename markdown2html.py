58c49c3ca2207f85e83aab2b7b9b0e86b8a9335d
OL-YAD <olyadtemesgen@gmail.com>
2023-01-15 13:54:34 +0300

    markdown

--- markdown2html.py
+++ markdown2html.py
@@ -1,43 +1,32 @@
 #!/usr/bin/python3
-'''
-A script that codes markdown to HTML
-'''
+"""markdown to html"""
+
+
 import sys
 import os
-import re
 
-if __name__ == '__main__':
+if __name__ == "__main__":
+    def errprint(*args):
+        """print errors"""
+        print(*args, file=sys.stderr)
 
-    # Test that the number of arguments passed is 2
-    if len(sys.argv[1:]) != 2:
-        print('Usage: ./markdown2html.py README.md README.html',
-              file=sys.stderr)
-        sys.exit(1)
+    if len(sys.argv) < 3:
+        errprint("Usage: ./markdown2html.py README.md README.html")
+        exit(1)
 
-    # Store the arguments into variables
-    input_file = sys.argv[1]
-    output_file = sys.argv[2]
+    if not os.path.exists(sys.argv[1]):
+        errprint("Missing {}".format(sys.argv[1]))
+        exit(1)
 
-    # Checks that the markdown file exists and is a file
-    if not (os.path.exists(input_file) and os.path.isfile(input_file)):
-        print(f'Missing {input_file}', file=sys.stderr)
-        sys.exit(1)
+    mdfile = open(sys.argv[1], 'r')
+    htmlfile = open(sys.argv[2], 'w')
+    with mdfile as md:
+        with htmlfile as html:
+            for line in mdfile:
+                lenOfLine = len(line)
+                heading = line.lstrip("#")
+                numOfHeading = lenOfLine - len(heading)
 
-    with open(input_file, encoding='utf-8') as file_1:
-        html_content = []
-        md_content = [line[:-1] for line in file_1.readlines()]
-        for line in md_content:
-            heading = re.split(r'#{1,6} ', line)
-            if len(heading) > 1:
-                # Compute the number of the # present to
-                # determine heading level
-                h_level = len(line[:line.find(heading[1])-1])
-                # Append the html equivalent of the heading
-                html_content.append(
-                    f'<h{h_level}>{heading[1]}</h{h_level}>\n'
-                )
-            else:
-                html_content.append(line)
-
-    with open(output_file, 'w', encoding='utf-8') as file_2:
-        file_2.writelines(html_content)
+                if 1 <= numOfHeading <= 6:
+                    line = "<h{}>".format(numOfHeading) + heading.strip() + "</h{}>\n".format(numOfHeading)
+                    html.write(line)
